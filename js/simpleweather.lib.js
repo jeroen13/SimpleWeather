6fa8af0254a376946efd5c19c57b8eba6638f35d
jeroen13 <jeroen_13@crackberry.com>
2014-06-17 19:18:14 +0200

    API Alpha v1.0

--- js/simpleweather.lib.js
+++ js/simpleweather.lib.js
@@ -3,8 +3,37 @@
 
 //global api_url
 var api_url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='
+var api_url_gcw = 'http://api.openweathermap.org/data/2.5/weather?q='
 
 var SimpleWeather = {
+	GetCurrentWeather: function(name, location, units, language, timeout, onSuccess, onError){
+		console.log('[SW.lib] GetCurrentWeather("'+name+'", "'+location+'", "'+units+'", "'+language+'", "'+timeout+'", "'+onSuccess+'", "'+onError+'")');
+		var GCWweather_url = api_url_gcw + location + '&mode=json&units=' + units + '&cnt=1&lang=' + language ;
+		console.log('[SW.lib] '+GCWweather_url);
+		var GCWSW_name = name;
+		var GCWSW_timeout = timeout;
+		var GCWoS = onSuccess;
+		var GCWoE = onError;
+		$.ajax({
+        		type : "GET",
+        		dataType : "jsonp",  
+ 		 		url: GCWweather_url, 
+ 				success: function(response){
+ 					console.log('[SW.lib] GET = Success');
+                    localStorage.setItem('SW.lib.' + GCWSW_name, JSON.stringify({timestamp:(new Date()).getTime(), data: response}));
+ 					console.log('[SW.lib] Stored Cache for "' + GCWSW_name + '"');
+ 					console.log('[SW.lib] Calling onSuccess function: "'+GCWoS+'()"');
+ 					window[GCWoS]();
+ 				}, 
+  				timeout: GCWSW_timeout,
+  				error: function(e){
+  					throw new Error('[SW.lib] ' + e);
+  					throw new Error('[SW.lib] ERROR @ fetching weather - Connection timed out');
+  					throw new Error('[SW.lib] Calling onError function: "'+GCWoE+'()"');
+ 					window[GCWoE]();
+  				} 
+			}); 
+	},
 	GetWeather: function(name, location, count, units, language, timeout, onSuccess, onError){
 		console.log('[SW.lib] GetWeather("'+name+'", "'+location+'", "'+count+'", "'+units+'", "'+language+'", "'+timeout+'", "'+onSuccess+'", "'+onError+'")');
 		var weather_url = api_url + location + '&mode=json&units=' + units + '&cnt=' + count + '&lang=' + language ;
